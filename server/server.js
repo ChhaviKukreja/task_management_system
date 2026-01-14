1cb7765f196c0a5ef63f01c979d4b433d3c2db8d
ChhaviKukreja <chhavikukreja19@gmail.com>
2026-01-15 00:09:14 +0530

    fix: update cors with exact vercel url

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "task-management-system-omega-one.vercel.app"],
+  origin: ["http://localhost:5173", "https://task-management-system-omega-one.vercel.app"],
   credentials: true
 }));
 
